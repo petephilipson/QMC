68e7deb8bc97d4be5dd58fe28dff2b06a633c20c
Pete Philipson <peter.philipson1@newcastle.ac.uk>
2020-02-12 15:45:40 +0000

    Uploaded bivariate and trivariate cases
    R files to generate the simulation studies used in the paper.

---
+++ simulation_2.R
@@ -0,0 +1,157 @@
+# Simulation study for trivariate model
+library(joineRML)
+library(reshape2)
+library(ggplot2)
+
+# Specification of 'true' parameters
+
+beta <- rbind(c(0, 1, 1, 1),
+              c(0, -1, 0, 0.5),
+              c(0, 0, 0.5, -0.5))
+D <- diag(6)
+D[1, 1] <- D[3, 3] <- D[5, 5] <- 0.5^2
+D[2, 2] <- D[4, 4] <- D[6, 6] <- 0.2^2
+D[1, 3] <- D[3, 1] <- -0.5 * 0.5 * 0.5
+D[1, 5] <- D[5, 1] <- 0.5^3
+D[3, 5] <- D[5, 3] <- -0.5*(0.5^2)
+all(eigen(D)$values > 0) # check covarinace matrix is PD
+
+sigma2 <- c(0.25, 0.25, 0.25)
+gamma.x <- c(0, 1)
+gamma.y <- c(-0.5, 1, 0.5)
+
+ltri <- lower.tri(D, diag = TRUE)
+theta <- c(D[lower.tri(D, diag = TRUE)], as.numeric(t(beta)), 
+           sigma2, gamma.x, gamma.y)
+
+#------------------------------------------------------------------------
+
+# Fit a joint model to a single simulated dataset given the number of patients
+
+simFit <- function(n, ...) {
+
+  # Simulate a dataset
+  sim <- simData(n = n, ntms = 6, beta = beta, D = D, sigma2 = sigma2,
+                 gamma.x = gamma.x, gamma.y = gamma.y,
+                 theta0 = -3.5, theta1 = 0.25, censlam = 0.05)
+
+  # Fit the joint model (vanilla MC)
+  fit_sim1 <- mjoint(
+    formLongFixed = list("Y1" = Y.1 ~ time + ctsxl + binxl,
+                         "Y2" = Y.2 ~ time + ctsxl + binxl,
+                         "Y3" = Y.3 ~ time + ctsxl + binxl),
+    formLongRandom = list("Y1" = ~ time | id,
+                          "Y2" = ~ time | id,
+                          "Y3" = ~ time | id),
+    formSurv = Surv(survtime, cens) ~ ctsx + binx,
+    data = sim$longdat,
+    survData = sim$survdat,
+    timeVar = "time",
+    control = list(type = "montecarlo", nMCmax = 12000, burnin = 5,
+                   tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5),
+    verbose = FALSE,
+    ...)
+
+  # Fit the joint model (antithetic MC)
+  fit_sim2 <- mjoint(
+    formLongFixed = list("Y1" = Y.1 ~ time + ctsxl + binxl,
+                         "Y2" = Y.2 ~ time + ctsxl + binxl,
+                         "Y3" = Y.3 ~ time + ctsxl + binxl),
+    formLongRandom = list("Y1" = ~ time | id,
+                          "Y2" = ~ time | id,
+                          "Y3" = ~ time | id),
+    formSurv = Surv(survtime, cens) ~ ctsx + binx,
+    data = sim$longdat,
+    survData = sim$survdat,
+    timeVar = "time",
+    control = list(type = "antithetic", nMCmax = 12000, burnin = 5,
+                   tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5),
+    verbose = FALSE,
+    ...)
+
+  # Fit the joint model (QMC with Sobol)
+  fit_sim3 <- mjoint(
+    formLongFixed = list("Y1" = Y.1 ~ time + ctsxl + binxl,
+                         "Y2" = Y.2 ~ time + ctsxl + binxl,
+                         "Y3" = Y.3 ~ time + ctsxl + binxl),
+    formLongRandom = list("Y1" = ~ time | id,
+                          "Y2" = ~ time | id,
+                          "Y3" = ~ time | id),
+    formSurv = Surv(survtime, cens) ~ ctsx + binx,
+    data = sim$longdat,
+    survData = sim$survdat,
+    timeVar = "time",
+    control = list(type = "sobol", nMCmax = 12000, burnin = 5,
+                   tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5),
+    verbose = FALSE,
+    ...)
+
+  return(list("vanilla" = fit_sim1,
+              "antithetic" = fit_sim2,
+              "qmc" = fit_sim3))
+
+}
+
+#------------------------------------------------------------------------
+
+# Run the simulation (serial mode)
+
+date()
+N <- 10 # number of simulations
+n <- 1000 # number of subjects
+
+ests <- array(dim = c(length(theta), N, 3))
+ses <- array(dim = c(length(theta), N, 3))
+props <- array(dim = c(7, N, 3))
+
+set.seed(12345)
+
+for (i in 1:N) {
+
+  # Run simulation
+  print(paste("Iteration:", i))
+  x <- simFit(n = n)
+
+  # Extract estimates
+  parFun <- function(u) {
+    with(u$coefficients, c(D[lower.tri(D, diag = TRUE)], beta, sigma2, gamma))
+  }
+  ests[, i, 1] <- parFun(x[[1]])
+  ests[, i, 2] <- parFun(x[[2]])
+  ests[, i, 3] <- parFun(x[[3]])
+
+  # Extract SEs
+  seFun <- function(u) {
+    sqrt(diag((vcov(u))))
+  }
+  ses[, i, 1] <- seFun(x[[1]])
+  ses[, i, 2] <- seFun(x[[2]])
+  ses[, i, 3] <- seFun(x[[3]])
+
+  # Extract properties
+  propFun <- function(u) {
+    c(as.numeric(u$comp.time[2], units = "secs"),
+      ncol(u$history),
+      u$finalnMC,
+      u$conv,
+      u$sfit$nevent / u$sfit$n, # avg. event rate
+      u$dims$nk[1] / u$dims$n, # avg. number of measurements
+      mean((u$sfit$y[, 1] < 6) & (u$sfit$y[, 2] == 0)) # avg. censoring before end of follow-up
+      )
+  }
+  props[, i, 1] <- propFun(x[[1]])
+  props[, i, 2] <- propFun(x[[2]])
+  props[, i, 3] <- propFun(x[[3]])
+
+  dimnames(ses)[[1]] <- names(diag(vcov(x[[1]])))
+  dimnames(ests)[[1]] <- dimnames(ses)[[1]]
+  dimnames(props)[[1]] <- c("time", "iter", "nMC", "conv", "event_rate", "avg_meas", "cens")
+
+  dimnames(ses)[[3]] <- c("vanilla", "antithetic", "qmc")
+  dimnames(ests)[[3]] <- dimnames(ses)[[3]]
+  dimnames(props)[[3]] <- dimnames(ses)[[3]]
+  
+  gc()
+
+}
+date()

6544d010b201cb8459d651296326cfede840f2f5
Pete Philipson <peter.philipson1@newcastle.ac.uk>
2020-02-20 16:12:17 +0000

    R code to fit models to PBC data in application
    R code to fit bivariate and trivariate joint models to the PBC data for each MC approach.

---
+++ QMCapplication.R
@@ -0,0 +1,82 @@
+# Run applications to PBC data
+library(joineRML)
+data("pbc2")
+# Log transform for bilirubin
+pbc2$log.b <- log(pbc2$serBilir)
+pbc2$trans.pro <- (0.1 * pbc2$prothrombin)^-4
+
+# BIVARIATE models
+# Vanilla fit
+fit.joineRML_biv <- mjoint(
+  formLongFixed = list("log.bil" = log.b ~ year + age + drug, "alb" = albumin ~ year + age + drug),
+  formLongRandom = list("log.bil" = ~ year | id, "alb" = ~ year | id),
+  formSurv = Surv(years, status2) ~ age + drug,
+  data = pbc2,
+  timeVar = "year", control = list(type = "montecarlo", nMCmax = 12000, burnin = 5,
+                                   tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5))
+# Antithetic fit
+fit.joineRML2_biv <- mjoint(
+  formLongFixed = list("log.bil" = log.b ~ year + age + drug, "alb" = albumin ~ year + age + drug),
+  formLongRandom = list("log.bil" = ~ year | id, "alb" = ~ year | id),
+  formSurv = Surv(years, status2) ~ age + drug,
+  data = pbc2,
+  timeVar = "year", control = list(type = "antithetic", nMCmax = 12000, burnin = 5,
+                                   tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5))
+# Quasi MC
+fit.joineRML3_biv <- mjoint(
+  formLongFixed = list("log.bil" = log.b ~ year + age + drug, "alb" = albumin ~ year + age + drug),
+  formLongRandom = list("log.bil" = ~ year | id, "alb" = ~ year | id),
+  formSurv = Surv(years, status2) ~ age + drug,
+  data = pbc2,
+  timeVar = "year", control = list(type = "sobol", nMCmax = 12000, burnin = 5,
+                                   tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5))
+
+# Extract time
+as.numeric(fit.joineRML_biv$comp.time[2], units = "secs")
+as.numeric(fit.joineRML2_biv$comp.time[2], units = "secs")
+as.numeric(fit.joineRML3_biv$comp.time[2], units = "secs")
+
+# Bootstrap - ncores subject to change
+boot1 <- bootSE2(fit.joineRML_biv, nboot = 100, ncores = 4, control = list(type = "montecarlo", nMCmax = 12000, burnin = 5,
+                                                                        tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5))
+boot2 <- bootSE2(fit.joineRML2_biv, nboot = 100, ncores = 4, control = list(type = "antithetic", nMCmax = 12000, burnin = 5,
+                                                                         tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5))
+boot3 <- bootSE2(fit.joineRML3_biv, nboot = 100, ncores = 4, control = list(type = "sobol", nMCmax = 12000, burnin = 5,
+                                                                         tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5), safe.boot = TRUE)
+
+# TRIVARIATE models
+# Use same set-up as in BMC paper
+# Covariate are log(bilirubin), albumin and transformed prothrombin
+# Vanilla fit
+fit.joineRML_tri <- mjoint(
+  formLongFixed = list("log.bil" = log.b ~ year + age + drug, "alb" = albumin ~ year + age + drug , 
+                       "trans.pro" = trans.pro ~ year + age + drug),
+  formLongRandom = list("log.bil" = ~ year | id, "alb" = ~ year | id, "trans.pro" = ~ year | id),
+  formSurv = Surv(years, status2) ~ age + drug, data = pbc2, timeVar = "year",
+  control = list(type = "montecarlo", nMCmax = 12000, burnin = 5,
+                 tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5))
+  #control = list(tol0 = 0.001, burnin = 400))MC
+# Antithetic fit
+fit.joineRML2_tri <- mjoint(
+  formLongFixed = list("log.bil" = log.b ~ year + age + drug, "alb" = albumin ~ year + age + drug , 
+                       "trans.pro" = trans.pro ~ year + age + drug),
+  formLongRandom = list("log.bil" = ~ year | id, "alb" = ~ year | id, "trans.pro" = ~ year | id),
+  formSurv = Surv(years, status2) ~ age + drug, data = pbc2, timeVar = "year",
+  control = list(type = "antithetic", nMCmax = 12000, burnin = 5,
+                 tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5))
+# Quasi MC
+fit.joineRML3_tri <- mjoint(
+  formLongFixed = list("log.bil" = log.b ~ year + age + drug, "alb" = albumin ~ year + age + drug , 
+                       "trans.pro" = trans.pro ~ year + age + drug),
+  formLongRandom = list("log.bil" = ~ year | id, "alb" = ~ year | id, "trans.pro" = ~ year | id),
+  formSurv = Surv(years, status2) ~ age + drug, data = pbc2, timeVar = "year",
+  control = list(type = "sobol", nMCmax = 12000, burnin = 5,
+                 tol0 = 1e-03, tol2 = 5e-03, nMCscale = 5))
+
+# Extract SEs from a generic fitted object
+seFun <- function(u) {
+  sqrt(diag((vcov(u))))
+}
+seFun(fitPBC_AMC)
+
+
